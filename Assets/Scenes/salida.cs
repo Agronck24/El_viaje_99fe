f167e47a447854765982fd18df1766cc973c5cde
Eric.H24 <eric.hernandez@enti.cat>
2021-05-31 23:46:24 +0200

    ASSETS6

---
+++ Assets/Scenes/salida.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class salida : MonoBehaviour
+{
+  
+
+    
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/ActivarPoder1.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivarPoder1 : MonoBehaviour
+{
+   public static bool dashPause = false;
+    public PlayerControler2D kitsune;
+
+     public GameObject dashMenu;
+
+       void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(dashPause)
+            {
+                Resume();
+            }
+        }
+    }
+
+ 
+
+     void OnTriggerEnter2D(Collider2D obj){
+        if(obj.tag=="Player"){
+           dashMenu.SetActive(true);
+           Time.timeScale = 0f;
+           dashPause = true;
+          
+           kitsune.poder1=true;
+           gameObject.SetActive(false);
+            
+
+        }
+     }  
+      public void Resume ()
+    {
+        dashMenu.SetActive(false);
+        Time.timeScale = 1f;
+        dashPause = false;
+    }
+
+
+     
+     
+}
