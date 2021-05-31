0ab4b3ee212df00011f32330df9d612cc818427e
Eric.H24 <eric.hernandez@enti.cat>
2021-05-31 23:59:52 +0200

    ASSETS7

---
+++ Assets/Background Levels/Lvl-2/Kodama-Speaking/KODOMO.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KODOMO : MonoBehaviour
+{
+  public PlayerControler2D player;
+  private Animator anim;
+
+  public GameObject text;
+
+     private  void Start()
+    {
+        {
+            anim=GetComponent<Animator>();
+          
+        }
+    }
+
+  
+
+      void OnTriggerEnter2D(Collider2D obj){
+        if(obj.tag=="Player"){
+             
+              anim.SetBool("SPIK", true);    
+              text.SetActive(true); 
+            
+        }else{
+             anim.SetBool("SPIK", false);
+        }
+    }
+    
+}
---
+++ Assets/Menu/MenuPausa.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+   public static bool GamePause = false;
+   
+   public GameObject pauseMenu;
+
+    public static bool dashPause = false;
+    
+
+     public GameObject dashMenu;
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("AAA");
+            if(GamePause==true)
+            {
+                Resume();
+            }else{
+                Pause();
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(GamePause=true)
+            {
+                Resume1();
+            }
+        }
+    }
+
+
+
+
+
+    public void Resume ()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GamePause = false;
+    }
+
+    public void Resume1 ()
+    {
+        dashMenu.SetActive(false);
+        Time.timeScale = 1f;
+        dashPause = false;
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GamePause = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MAIN MENU");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
