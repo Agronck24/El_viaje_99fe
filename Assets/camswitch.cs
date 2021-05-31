8fbf3e3c36beaee2cf00ee94364179e1638c74d3
Eric.H24 <eric.hernandez@enti.cat>
2021-05-31 23:38:49 +0200

    ASSETS1

---
+++ Assets/camswitch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camswitch : MonoBehaviour
+{
+   public bool cam;
+     private Animator anim;
+
+     public PlayerControler2D playercam;
+ private  void Start()
+    {
+        {
+            anim=GetComponent<Animator>();
+          
+        }
+    }
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && playercam.cam==true){
+            
+            anim.SetBool("AAA", true);
+        }
+        if(Input.GetKeyDown(KeyCode.F) && playercam.cam==true){
+            
+            anim.SetBool("AAA", false);
+        }
+    }
+}
