55f9661cba786a2b903268227a6325e19c18a728
Kuroha <samantha.bello@enti.cat>
2021-06-01 23:28:51 +0200

    Lvl2

---
+++ Assets/Scripts3/LightLvl2Fade.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightLvl2Fade : MonoBehaviour
+{
+
+    public PlayerControler2D player;
+    private Animator anim;
+
+    public GameObject text;
+
+    private void Start()
+    {
+        {
+            anim = GetComponent<Animator>();
+
+        }
+    }
+
+   /* void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.tag == "Player")
+        {
+
+            anim.SetBool("SPIK", true);
+            text.SetActive(true);
+
+        }
+        else
+        {
+            anim.SetBool("SPIK", false);
+        }
+    }
+    */
+}
