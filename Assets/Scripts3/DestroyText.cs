6ab9847972595fcad97fc11ccd9e2241c74c2196
Kuroha <samantha.bello@enti.cat>
2021-06-02 20:54:48 +0200

    Kodama tuto fix

--- Assets/Scripts3/DestroyText.cs
+++ Assets/Scripts3/DestroyText.cs
@@ -13,6 +13,6 @@ public class DestroyText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Destroy(gameObject, 3.5f);
+        Destroy(gameObject, 4f);
     }
 }
