fdad3e347a8ee8e67e08bacf1de62c2d79cbb82b
Kuroha <samantha.bello@enti.cat>
2021-06-03 14:03:26 +0200

    Fix Lvl 3 Manín & luz

---
+++ Assets/Scripts3/KodamaLVL3.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KodamaLVL3 : MonoBehaviour
+{
+    public PlayerControler2D player;
+    private Animator anim;
+
+    public GameObject text;
+
+    private void Start()
+    {
+        {
+            anim = GetComponent<Animator>();
+
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.tag == "Player")
+        {
+
+            anim.SetBool("SpeakLVL3", true);
+            text.SetActive(true);
+            Destroy(gameObject, 10);
+
+        }
+        else
+        {
+            anim.SetBool("SpeakLVL3", false);
+        }
+    }
+}
