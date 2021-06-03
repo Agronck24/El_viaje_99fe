cf9e5a86c9b1c9841117530203a1653a2c963c32
Kuroha <samantha.bello@enti.cat>
2021-06-03 13:02:16 +0200

    Fix alma madre tutorial

---
+++ Assets/Scripts3/AlmaMadreTuto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlmaMadreTuto : MonoBehaviour
+{
+    public PlayerControler2D player;
+
+
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.tag == "Player")
+        {
+            Destroy(gameObject, 0.3f);
+        }
+    }
+}
